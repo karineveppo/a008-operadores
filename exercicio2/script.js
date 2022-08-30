510e57c20af2aed0bd3637f30492f81370d018ea
karineveppo <karineveppo83@gmail.com>
2022-08-30 17:45:35 -0300

    exercios realizados

---
+++ exercicio2/script.js
@@ -0,0 +1,7 @@
+const numeroUm = Number(prompt("digite um numero"))
+const numeroDois = Number(prompt("digite outro numero"))
+
+console.log(numeroUm > numeroDois)
+console.log(numeroUm === numeroDois)
+console.log(numeroUm % numeroDois === 0)
+console.log(numeroDois % numeroUm === 0)
